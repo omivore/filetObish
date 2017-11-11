6542302eb0d8b73ddf541439851f25db47bcccf1
Jason Tan <10.1.19.15.14.tan@gmail.com>
2017-11-11 18:55:47 -0500

    Add form and no-op submission to index

--- frontend/filetobish/app/controllers/HomeController.java
+++ frontend/filetobish/app/controllers/HomeController.java
@@ -1,6 +1,11 @@
 package controllers;
 
-import play.mvc.*;
+import javax.inject.Inject;
+
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -8,14 +13,19 @@ import play.mvc.*;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    private final Form<TwitterUserData> form;
+
+    @Inject
+    public HomeController(FormFactory formFactory) {
+        this.form = formFactory.form(TwitterUserData.class);
+    }
+
     public Result index() {
         return ok(views.html.index.render());
+    }
+
+    public Result findUser() {
+        return ok(views.html.twitter.render(form));
     }
 
 }
---
+++ frontend/filetobish/app/models/TwitterUser.java
@@ -0,0 +1,11 @@
+package models;
+
+public class TwitterUser {
+    public String username;
+    public String password;
+
+    public TwitterUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
